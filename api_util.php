ef63f011825b2aa8547f2164224b131097f3e8f3
John Campbell <jpc@Johns-MacBook-Pro.local>
2016-05-23 12:54:46 -0500

    new mods

--- api_util.php
+++ api_util.php
@@ -189,30 +189,36 @@ class api_util {
         }
 
         return $table;
-    }
-
+    }  
+    
     /**
      * Convert a error object into nice text
      * @param Object $error simpleXmlObject
      * @return string formatted error message
      */
-    public static function xmlErrorToString($error) {
-
+    public static function xmlErrorToString($error,$multi=false) {
         if (!is_object($error)) {
             return "Malformed error: " . var_export($error, true);
         }
+        // show just the first error
+        //$error = $error->error[0];
+        foreach ($error->error as $error) {
+            if (!is_object($error)) {
+                return "Malformed error: " . var_export($error, true);
+            }
 
-        $error = $error->error[0];
-        if (!is_object($error)) {
-            return "Malformed error: " . var_export($error, true);
+            $errorno = is_object($error->errorno) ? (string)$error->errorno : ' ';
+            $description = is_object($error->description) ? (string)$error->description : ' ';
+            $description2 = is_object($error->description2) ? (string)$error->description2 : ' ';
+            $correction = is_object($error->correction) ? (string)$error->correction : ' ';
+            $error_string .= "$errorno: $description: $description2: $correction\n";
+            if ($multi === false) {
+                break;
+            }
         }
-
-        $errorno = is_object($error->errorno) ? (string)$error->errorno : ' ';
-        $description = is_object($error->description) ? (string)$error->description : ' ';
-        $description2 = is_object($error->description2) ? (string)$error->description2 : ' ';
-        $correction = is_object($error->correction) ? (string)$error->correction : ' ';
-        return "$errorno: $description: $description2: $correction";
+        return $error_string;
     }
+
 
 
 }
